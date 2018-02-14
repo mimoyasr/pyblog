27066c893d146acf0a3fffb053d55edd836d8dd7
maiFouad94 <mai_elrefaey@yahoo.com>
2018-02-14 17:43:55 +0200

    el app

--- pyblog/db.py
+++ pyblog/db.py
@@ -6,7 +6,7 @@ DATABASES = {
        'ENGINE': 'django.db.backends.mysql',
        'NAME': 'pyblog',
        'USER': 'root',
-       'PASSWORD': '1234',
+       'PASSWORD': '12345',
        'HOST': '127.0.0.1',
        'PORT': '3306',
    }
